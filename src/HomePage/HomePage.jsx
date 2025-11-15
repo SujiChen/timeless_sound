21dd6f5c73d0842ebc523d02f1c3a0d6a9ced9f8
SujiChen <sujichen0@gmail.com>
2025-11-15 16:47:42 -0500

    finish overlay design for both pages

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -4,28 +4,25 @@ import Header from '../Header/Header';
 import RetroPage from "../Retro/RetroPage";
 import ModernPage from "../Modern/ModernPage";
 
-
-
 export default function HomePage() {
-    const [ratio, setRatio] = useState(50);
-    const handleSlide = (e) => {
-        setRatio(e.target.value);
-    };
+    const [ratio, setRatio] = useState(50); // 50% by default
 
-    return(
-        <div className="home-container">
-            
-        {/* <Header/> */}
-
-        <div className="retro-half" style={{ width: `${ratio}%` }}>
-            <RetroPage />
-        </div>
-
-      <div className="modern-half" style={{ width: `${100 - ratio}%` }}>
+  return (
+    <div className="overlay-container">
+      {/* Modern Page at the back */}
+      <div className="page modern">
         <ModernPage />
       </div>
 
-      {/* Slider Control */}
+      {/* Retro Page on top, width controlled by slider */}
+      <div
+        className="page retro"
+        style={{ width: `${ratio}%` }}
+      >
+        <RetroPage />
+      </div>
+
+      {/* Slider */}
       <input
         type="range"
         min="0"
@@ -34,7 +31,6 @@ export default function HomePage() {
         className="slider"
         onChange={(e) => setRatio(e.target.value)}
       />
-        </div>
+    </div>
     )
-
 }
